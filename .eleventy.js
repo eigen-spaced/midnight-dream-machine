77d18dd1f13d61ea9f1fe22dbb0e7899307c1a16
Magnuscake <saneyar.k@gmail.com>
2023-07-02 23:12:41 +1000

    Config changes Add the _site/ folder to .gitignore. Also, since we've moved all our content to content/, the eleventy config accomodates these new changes

--- .eleventy.js
+++ .eleventy.js
@@ -12,10 +12,37 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd-LL-yyyy");
+  });
+
   // Output directory: _site
   eleventyConfig.addPassthroughCopy("static");
 
-  eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("posts/**/*.md");
-  });
+  // eleventyConfig.addCollection("posts", function (collectionApi) {
+  //   return collectionApi.getFilteredByGlob("posts/**/*.md");
+  // });
+
+  // eleventyConfig.addFilter("getAllTags", (collection) => {
+  //   let tagSet = new Set();
+  //   for (let item of collection) {
+  //     (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+  //   }
+  //   return Array.from(tagSet);
+  // });
+  //
+  // eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+  //   return (tags || []).filter(
+  //     (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+  //   );
+  // });
+
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes", // default: "_includes"
+      data: "../_data", // default: "_data"
+      output: "_site",
+    },
+  };
 };
